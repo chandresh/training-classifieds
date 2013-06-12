6f1f9db3496b6cb9a129d0bcb4e8f8387a488452
Chandresh Pant <cp@cpant.in>
2013-06-12 06:55:33 +0530

    added user to adverts

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,3 +1,5 @@
 class Advert < ActiveRecord::Base
-  attr_accessible :category_id, :contact_details, :description, :title
+  attr_accessible :category_id, :contact_details, :description, :title, :user_id
+  belongs_to :category
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612011458) do
+ActiveRecord::Schema.define(:version => 20130612012148) do
 
   create_table "adverts", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130612011458) do
     t.integer  "category_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.integer  "user_id"
   end
 
   create_table "categories", :force => true do |t|
